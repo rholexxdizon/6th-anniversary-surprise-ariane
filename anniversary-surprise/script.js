fae9b15b74095191606bc3209f2dfcb9529bb7ee
rholexxdizon <dizonrholex02@gmail.com>
2024-06-06 16:11:34 +0800

    add script

--- anniversary-surprise/script.js
+++ anniversary-surprise/script.js
@@ -1,4 +1,29 @@
 //turn pages when click next or prev button
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if (pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+            }, 500)
+        }
+        else {
+            pageTurn.classList.add('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+            }, 500)
+        }
+    }
+    
+})
+
+
+
 
 //contact me button when click
 
